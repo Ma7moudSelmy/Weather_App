60a9d3cd00f07194c7497f8d83d4fc647d4703bf
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-07-24 21:37:32 +0300

    feat: Enhance main widget with app bar and background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(MyWidget());
-
+void main() {
+  runApp(MyWidget());
 }
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(237, 35, 188, 235),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            'Weather App',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
