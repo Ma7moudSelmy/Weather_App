3c43f595b7dfed29de1a3c05fad4066387e227d4
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-07-27 13:28:21 +0300

    feat: Update SearchView title and add TextField for city search input

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -7,8 +7,24 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search Weather'),
+        title: const Text('Search a city'),
         backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: BorderSide(
+                  color: const Color.fromARGB(255, 223, 129, 7),
+                  width: 2,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
