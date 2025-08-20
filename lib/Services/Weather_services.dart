7294627be0ab92c3c8fcf8bb83d6b5e406f60a85
Mahmoudselmy <Ma7moud.m.selmy1@gmail.com>
2025-08-20 22:41:17 +0300

    Refactor WeatherServices to improve error handling and enhance getCurrentWeather method with clearer response parsing and logging.

--- lib/Services/Weather_services.dart
+++ lib/Services/Weather_services.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:weather_app/Models/Weather_models.dart';
 
@@ -7,24 +6,30 @@ class WeatherServices {
   final Dio dio;
   final String baseUrl = "https://api.weatherapi.com/v1";
   final String apikey = "b428eb1dea214de3b92172514250608";
+
   WeatherServices({required this.dio});
 
-  Future getCurrentWeather({required String cityName}) async {
+  Future<WeatherModels> getCurrentWeather({required String cityName}) async {
     try {
-      Response response = await dio.get(
-        "$baseUrl/forecast.json?key=$apikey&q=$cityName&days=1",
+      final response = await dio.get(
+        "$baseUrl/forecast.json",
+        queryParameters: {"key": apikey, "q": cityName, "days": 1},
       );
+
       if (response.statusCode == 200) {
-        WeatherModels weatherModels = WeatherModels.fromJson(response.data);
-        return weatherModels;
+        return WeatherModels.fromJson(response.data);
+      } else {
+        throw Exception("Unexpected status code: ${response.statusCode}");
       }
     } on DioException catch (e) {
       final String errMessage =
           e.response?.data["error"]["message"] ??
-          'oops something went wrong please try again later';
+          "Oops, something went wrong. Please try again later.";
+      log("DioException: $errMessage");
+      throw Exception(errMessage);
     } catch (e) {
-      log(e.toString());
-      throw Exception("opps something went wrong please try again later");
+      log("General Exception: $e");
+      throw Exception("Oops, something went wrong. Please try again later.");
     }
   }
 }
